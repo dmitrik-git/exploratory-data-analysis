e5f637e26201adc0951e5624e98c7487d1a78b4f
Dmitri K <dmitri.kovagin@gmail.com>
2021-01-17 13:41:42 +0200

    swirl exercise

---
+++ week2-colors.R
@@ -0,0 +1,10 @@
+# grDevices package
+install.packages ("grDevices")
+library(grDevices)
+
+colors()
+colorRamp()
+colorRampPalette()
+rgb(0, .5, .5, .3) # the last argument set the transparency
+
+# RColorBrewer package
