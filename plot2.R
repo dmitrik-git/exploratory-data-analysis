6348e185f0ad3fdbccb452730459b3a53a7c7183
Dmitri K <dmitri.kovagin@gmail.com>
2021-01-26 16:36:31 +0200

    Question 2

---
+++ plot2.R
@@ -0,0 +1,31 @@
+# Download the file
+
+if (!file.exists("data")) {
+    dir.create("data")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileUrl, destfile = "./data/data.zip", method = "curl")
+
+install.packages("zip")
+library(zip)
+unzip("./data/data.zip", exdir = "./data/")
+
+# Load the data
+nei <- readRDS("./data/summarySCC_PM25.rds")
+scc <- readRDS("./data/Source_Classification_Code.rds")
+
+# Baltimore data
+nei.balt <- subset (nei, fips == "24510")
+
+# Prepare the data
+sumyears <- tapply(nei.balt$Emissions, nei.balt$year, sum)
+years <- names(sumyears)
+df <- data.frame(sumyears)
+df <- cbind (df, years)
+names(df) <- c("emissions","year")
+
+# Plotting
+png(file = "plot2.png")
+barplot(emissions ~ year , data = df, col = "blue", main = "Total PM2.5 emission in Baltimore from all sources")
+dev.off()
