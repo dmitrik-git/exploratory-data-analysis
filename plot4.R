9a57bb7b6d9f0554f5028495dff9fa71b861ff73
Dmitri K <dmitri.kovagin@gmail.com>
2021-01-26 21:37:37 +0200

    plot 4

---
+++ plot4.R
@@ -0,0 +1,31 @@
+# Download the file
+
+if (!file.exists("data")) {
+    dir.create("data")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileUrl, destfile = "./data/data.zip", method = "curl")
+
+install.packages("zip")
+library(zip)
+unzip("./data/data.zip", exdir = "./data/")
+
+# Load the data
+nei <- readRDS("./data/summarySCC_PM25.rds")
+scc <- readRDS("./data/Source_Classification_Code.rds")
+
+# Coal related emissions
+coal <- scc[grep("Coal", scc$EI.Sector),1]
+coal <- as.list(coal)
+nei.coal <- subset (nei, SCC %in% coal)
+
+
+# Prepare the data
+sumyears <- aggregate(Emissions ~ year, data = nei.coal, sum)
+
+
+# Plotting
+png(file = "plot4.png")
+barplot(Emissions ~ year , data = sumyears, col = "green")
+dev.off()
